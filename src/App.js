33c98fe10d48c0cc55d906315edfac467b313602
joshraiborde <joshraiborde@gmail.com>
2021-08-21 15:16:54 -0400

    added FunctionalComp and ClassComp to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import FunctionalComp  from "./Components/FunctionalComp";
+import ClassComp from './Components/ClassComp';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+    <h1>hi</h1>
+    <h1>Components</h1>
+    <FunctionalComp/>
+    <ClassComp/>
     </div>
   );
 }
