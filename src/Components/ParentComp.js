9a337cd2d2b0b721e9f11bea4e1a519ce5d7ecb7
joshraiborde <joshraiborde@gmail.com>
2021-08-22 18:13:22 -0400

    added regcomp and purecomp this.state.name

--- src/Components/ParentComp.js
+++ src/Components/ParentComp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import PureComp from './PureComp'
+import RegComp from './RegComp'
 class ParentComp extends Component {
     constructor(props) {
         super(props)
@@ -21,6 +22,8 @@ class ParentComp extends Component {
         return (
             <div>
                 Parent Comp
+                <RegComp name={this.state.name}/>
+                <PureComp name={this.state.name}/>
             </div>
         )
     }
