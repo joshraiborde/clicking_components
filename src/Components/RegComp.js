cfca1d07603c0ec47154495fc4cbdb71d0fdcc74
joshraiborde <joshraiborde@gmail.com>
2021-08-22 18:14:05 -0400

    added this.props.name

--- src/Components/RegComp.js
+++ src/Components/RegComp.js
@@ -4,7 +4,7 @@ class RegComp extends Component {
     render() {
         return (
             <div>
-                regular Component
+                regular Component {this.props.name}
             </div>
         )
     }
